13309d3e296132874e3db2c65cceb2d0de2f2439
Queenie07 <dasqueenie@gmail.com>
2017-09-14 04:51:35 +0530

    Updated structure

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.opacity=0.5;
@@ -9,3 +10,16 @@ function closeNav() {
     document.getElementById("main").style.opacity=1;
 
 }
+=======
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.opacity=0.5;
+
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.opacity=1;
+
+}
+>>>>>>> 95413184ac9957c2b9185feec7a760fc41c4e2fb
