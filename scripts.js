95413184ac9957c2b9185feec7a760fc41c4e2fb
Queenie Das <dasqueenie@gmail.com>
2017-09-14 03:33:31 +0530

    Basic Structure

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.opacity=0.5;
+
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.opacity=1;
+
+}
