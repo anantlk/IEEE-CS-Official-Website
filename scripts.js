3a9be41da84d4f547d7e182b2a5efee288a4b4b0
Queenie07 <dasqueenie@gmail.com>
2017-09-15 13:44:38 +0530

    Responsive

--- scripts.js
+++
@@ -1,6 +0,0 @@
-function nav_open() {
-    document.getElementById("mySidebar").style.display = "block";
-}
-function nav_close() {
-    document.getElementById("mySidebar").style.display = "none";
-}
