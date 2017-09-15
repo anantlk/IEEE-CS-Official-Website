af9ad658a574d72f770da54197b47690674bc00c
anantlk <anant.lk.1997@gmail.com>
2017-09-15 16:48:33 +0530

    update

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function nav_open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+function nav_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
